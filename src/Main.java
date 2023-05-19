607f1d5a5f06146751313bf91eba4c8322f3ee8d
Pivkina <ketrin_696@mail.ru>
2023-05-19 22:45:45 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        int ticket = 25485;
+        int bonus = 20;
+
+        System.out.println(ticket / bonus);
+
+
+    }
+}
